6848d4458156f631a1aa946ac1a4fdafcea0321e
Phillip <philgapp@gmail.com>
2021-04-03 13:25:19 +0200

    Database and Cleanup
    - Connected first query and mutation to MongoDB!
    -- Returning correctly, confirmed data is stored correctly so far.
    - Renamed createTestOption to createOption - to be used as the official mutation going forward :)
    - Removed unused db.collections (Spreads and Legs).
    - Removed documentation that is now held in an external doc.
    - Removed unused queries from static testing.
    - Removed unused consts from static testing.
    - Removed unused  mutations and resolvers from tutorial code.
    - Removed unnecessary console.logs.

--- src/server.js
+++ src/server.js
@@ -19,135 +19,17 @@ export const start = async () => {
 
         const Users = db.collection('users')
         const Options = db.collection('options')
-        const Spreads = db.collection('spreads')
-        const Legs = db.collection('legs')
         const UnderlyingTrades = db.collection('underlyingtrades')
         const UnderlyingHistories = db.collection('underlyinghistories')
 
-        const tests = [
-            {_id:'dsdfser',title:'Test 1',content:'Test 1 content'},
-            {_id:'dsdfser2',title:'Test 2',content:'Test 2 content'},
-            {_id:'dsdfser3',title:'Test 3',content:'Test 3 content'},
-            {_id:'dsdfser4',title:'Test 4',content:'Test 4 content'}
-        ]
-
-        /*
-        Playing with mutations from client side - adding option:
-
-        mutation {
-          createTestOption(input: {
-            userId: "temp1",
-            symbol: "TSLA",
-            type: P,
-            spreads: [
-              {
-                legs: [
-                  {
-                    qty: 1,
-                    isSpread: false,
-                    entryDate: 1617235200000,
-                    expirationDate: 1621555200000,
-                    strike: 22.5,
-                    underlyingEntryPrice: 24,
-                    initialPremium: 0.9,
-                    notes: "Just some random test notes to play with...",
-                  }
-                ]
-              }
-            ]
-          }) {
-              _id
-              symbol
-                type
-              spreads {
-                _id
-                legs {
-                  _id
-                  qty
-                  entryDate
-                  strike
-                  expirationDate
-                  initialPremium
-                  initialRoi
-                  initialAroi
-                  capitalRequirement
-                  notes
-                }
-              }
-            }
-        }
-
-
-        BETTER - parameter object pattern with variable
-
-        $input = {
-            userId: "temp1",
-            symbol: "TSLA",
-            type: P,
-            spreads: [
-              {
-                legs: [
-                  {
-                    qty: 1,
-                    isSpread: false,
-                    entryDate: 1617235200000,
-                    expirationDate: 1621555200000,
-                    strike: 22.5,
-                    underlyingEntryPrice: 24,
-                    initialPremium: 0.9,
-                    notes: "Just some random test notes to play with...",
-                  }
-                ]
-              }
-            ]
-          }
-
-          mutation createTestOption($input: OptionInput!) {
-              createTestOption(input: $input) {
-                  _id
-                  symbol
-                    type
-                  spreads {
-                    _id
-                    legs {
-                      _id
-                      qty
-                      entryDate
-                      strike
-                      expirationDate
-                      initialPremium
-                      initialRoi
-                      initialAroi
-                      capitalRequirement
-                      notes
-                    }
-                  }
-                  user {
-                    _id
-                    firstName
-                    email
-                  }
-                }
-            }
-
-
-        NOTES:
-        Dates. From client side always send and receive numbers, for example:
-        new Date("2021-05-21").getTime() = 1621555200000 (as used in mutation example above)
-
-         */
 
         const testUsers = [
             {_id:'temp1',firstName:'Phillip',lastName:'Gapp',email:'philgapp@gmail.com',authType:'LOCAL',password:'password'},
             {_id:'temp2',firstName:'Demo',lastName:'User',email:'demo@test.com',authType:'LOCAL',password:'password'}
         ]
 
-        const testOptions = []
-
         const typeDefs = [`
       type Query {
-        viewTest(_id: ID!): Test
-        viewTests: [Test]
         user(_id: ID): User
         users: [User]
         option(_id: String): Option
@@ -160,11 +42,6 @@ export const start = async () => {
         underlyingTrades: [UnderlyingTrade]
         underlyingHistory(_id: String): UnderlyingHistory
         underlyingHistories: [UnderlyingHistory]
-      }
-      type Test {
-        _id: ID!
-        title: String
-        content: String
       }
       type User {
         _id: ID!
@@ -255,10 +132,6 @@ export const start = async () => {
       }
       type Mutation {
         createOption(input: OptionInput): Option
-        createTestOption(input: OptionInput): Option
-        createSpread(optionId: String, content: String): Spread
-        createSpreadLeg(spreadId: String, content: String): Leg
-        createOptionLeg(optionId: String, content: String): Leg
         createUnderlyingTrade(underlingHistoryId: String) : UnderlyingTrade
         createUnderlyingHistory(ticker: String) : UnderlyingHistory
       }
@@ -301,29 +174,13 @@ export const start = async () => {
                     })
                     return res[0]
                 },
-                viewTest: async (root, args) => {
-                    const id = args._id
-                    const res = tests.filter(function(test) {
-                        return test._id === id
-                    })
-                    return res[0]
-                },
-                viewTests: () => {
-                    return tests
-                },
                 option: async (root, {_id}) => {
                     return prepare(await Options.findOne(ObjectId(_id)))
                 },
                 getOptionsByUser: async (root, args) => {
                     const userId = args._id
-                    console.log(userId)
-                    console.log(testOptions)
-                    const res = testOptions.filter(function(option) {
-                        option.userId === userId
-                    })
-                    console.log(res)
+                    const res = (await Options.find({ userId: { $eq: userId } }).toArray()).map(prepare)
                     return res
-                    //return (await Options.find({}).toArray()).map(prepare)
                 },
                 spread: async (root, {_id}) => {
                     return prepare(await Spreads.findOne(ObjectId(_id)))
@@ -338,29 +195,15 @@ export const start = async () => {
                   return res[0]
               }
             },
-            /*Post: {
-                comments: async ({_id}) => {
-                    return (await Comments.find({postId: _id}).toArray()).map(prepare)
-                }
-            },
-            Comment: {
-                post: async ({postId}) => {
-                    return prepare(await Posts.findOne(ObjectId(postId)))
-                }
-            },*/
             Mutation: {
-                createOption: async (root, args, context, info) => {
-                    const res = await Options.insertOne(args)
-                    return prepare(res.ops[0])  // https://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#~insertOneWriteOpResult
-                },
-                createTestOption: async (root, args) => {
+                createOption: async (root, args) => {
                     const inputData = args.input
                     // TODO Validate current user....
-                    // generateIds for Option, Spread(s) and Leg(s)
-                    inputData._id = generateTestId
-                    inputData.spreads[0]._id = generateTestId
+                    // generate ObjectIds for Option, Spread(s) and Leg(s)
+                    inputData._id = new ObjectId
+                    inputData.spreads[0]._id = new ObjectId
                     const thisLeg = inputData.spreads[0].legs[0]
-                    thisLeg._id = generateTestId
+                    thisLeg._id = new ObjectId
                     // Set variables and calculate ROI and AROI
                     const capitalRequirement = thisLeg.strike
                     const initialPremium = thisLeg.initialPremium
@@ -370,13 +213,14 @@ export const start = async () => {
                     thisLeg.initialRoi = initialRoi
                     thisLeg.initialAroi = calculateAroi({startDate:startDate,endDate:endDate,roi:initialRoi})
                     thisLeg.capitalRequirement = capitalRequirement * 100
-                    testOptions.push(inputData)
-                    return inputData
+                    try {
+                        const res = await Options.insertOne(inputData)
+                        return prepare(res.ops[0])
+                    }
+                    catch (e) {
+                        return e
+                    }
                 },
-                /*createComment: async (root, args) => {
-                    const res = await Comments.insert(args)
-                    return prepare(await Comments.findOne({_id: res.insertedIds[1]}))
-                },*/
             },
             Date: new GraphQLScalarType({
                 name: 'Date',
